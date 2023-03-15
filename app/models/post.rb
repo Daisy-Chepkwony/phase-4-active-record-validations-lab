19e7a9884d1622477e949cea0c643351ae01b2e5
+ <+>
2023-03-15 10:48:23 +0300

    commit tree c0945b210ace5ca84fc5c8ae94bc19e2eb3ca6eb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
+class ClickbaitValidator < ActiveModel::Validator
+    def validate(record)
+      unless record.title =~ /(Won't Believe|Secret|Top \d+|Guess)/
+        record.errors[:title] << "must be sufficiently clickbait-y"
+      end
+    end
+  end
+
+
 class Post < ApplicationRecord
+    validates :title, presence: true, clickbait: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    # validates_with ClickbaitValidator
+
+    # def validate(record)
+    #     unless record.title =~ /(Won't Believe|Secret|Top \d+|Guess)/
+    #       record.errors[:title] << "must be sufficiently clickbait-y"
+    #     end
+    #   end
 end
